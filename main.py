803ab97f104f388a91734489261b2b20e2d18e1c
Aaditya Suri <84222486+AadityaSuri@users.noreply.github.com>
2021-05-16 20:14:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ playerShip = pygame.transform.scale(playerShip, (180, 120))
 enemyShip = pygame.image.load("Game Assets/EnemyShip.png")
 enemyShip = pygame.transform.scale(enemyShip, (150, 100))
 playerBullet = pygame.image.load("Game Assets/PlayerBullet.png")
+playerBullet = pygame.transform.scale(playerBullet, (20,20))
 enemyBullet = pygame.image.load("Game Assets/EnemyBullet.png")
 bg = pygame.image.load("Game Assets/Background.jpg")
 bg = pygame.transform.scale(bg, (width, height))
@@ -30,15 +31,26 @@ enemyY = random.randint(0, 100)
 enemyX_change = 2.3
 enemyY_change = 0.3
 
+#player bullet coordinates
+pBulletX = 0
+pBulletY = 600
+pBulletY_change = 6
+pBulletState = "ready"
+
 def player(x, y):
     screen.blit(playerShip, (x, y))
 
 def enemy(x, y):
     screen.blit(enemyShip, (x, y))
 
-#event loop
-running = True
+def firePBullet(x, y):
+    global pBulletState
+    pBulletState = "fire"
+    screen.blit(playerBullet,(x + 80, y + 25))
 
+#event loop
+
+running = True
 while running:
     screen.blit(bg, (0, 0))
 
@@ -52,6 +64,10 @@ while running:
                 playerX_change = -2
             if event.key == pygame.K_RIGHT:
                 playerX_change = 2
+            if event.key == pygame.K_SPACE:
+                if pBulletState == "ready":
+                    pBulletX = playerX
+                    firePBullet(playerX, pBulletY)
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -68,9 +84,17 @@ while running:
     enemyX += enemyX_change
     enemyY += enemyY_change
     if enemyX <= -20:
-        enemyX_change = 2.3
+        enemyX_change = 2.2
     if enemyX >= width - 130:
         enemyX_change = -2.3
+
+    #bullet movement
+    if pBulletY <= -30:
+        pBulletY = 600
+        pBulletState = "ready"
+    if pBulletState == "fire":
+        firePBullet(pBulletX, pBulletY)
+        pBulletY -= pBulletY_change
 
     player(playerX, playerY)
     enemy(enemyX, enemyY)
